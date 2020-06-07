e6c4b0c9175144aff029fff3d7f664eed2a73b7a
Duarte Ferreira Dias <dudias@microsoft.com>
2020-06-08 00:20:58 +0100

    get list of products in search page fully working

--- main.py
+++ main.py
@@ -14,26 +14,27 @@ class Scraper:
         self.headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
 
+    # Working
+    # Gets links of all product in the page
     def getItemLinksInPage(self):
         itemsUrls = []
         ## Make URL of search results
         URL = 'https://www.amazon.com/' + self.searchTerm.replace(' ', '-') + '/s?k=' + self.searchTerm.replace(' ', '+') + '&page=' + str(self.currentPage)
-        print(URL)
+        # Get page DOM
         dom = requests.get(URL, headers = self.headers).text
         soup = BeautifulSoup(dom)
+        
+        # Get all product's outer div
         productDivs = soup.findAll("div", {"class": "a-section a-spacing-medium"})
         
+        # Get each product's <a> tag and its respective href
         for productDiv in productDivs:
             child = productDiv.findAll("a", {"class": "a-link-normal a-text-normal"}, href = True)
-            print(child)
             if child:
-                itemsUrls.append(productDiv.findChildren("a", {"class": "a-link-normal a-text-normal"}))
+                for subChild in child:
+                    itemsUrls.append('https://www.amazon.com' + subChild['href'])
 
-
-        ## Test html page result
-        with open('search.html', 'w') as f:
-            print(soup.encode('utf-8'), file=f)
-
+        # Return list of product urls
         return itemsUrls
 
 
